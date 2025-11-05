ef4282ecc4d6f48db0953e715775b378943a9f29
Evanildo89 <vany_casal@hotmail.com>
2025-11-05 18:08:32 +0000

    Corrigido router de services

--- landing_app/services/urls.py
+++ landing_app/services/urls.py
@@ -1,16 +1,26 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ServiceViewSet, BookingViewSet, DisponibilidadeView, ProfessionalListAPIView, criar_reserva, index, booking_detail, enviar_confirmacao_email, enviar_cancelamento_email, enviar_reagendamento_email, submit_review
+from .views import (
+    ServiceViewSet,
+    BookingViewSet,
+    DisponibilidadeView,
+    ProfessionalListAPIView,
+    criar_reserva,
+    booking_detail,
+    enviar_confirmacao_email,
+    enviar_cancelamento_email,
+    enviar_reagendamento_email,
+    submit_review,
+)
 
 router = DefaultRouter()
 router.register(r'services', ServiceViewSet, basename='service')
 router.register(r'bookings', BookingViewSet, basename='booking')
 
 urlpatterns = [
-    path('index/', index),  # ou '' se quiser que seja só o domínio principal
-    path('api/', include(router.urls)),
-    path('disponibilidade/<int:service_id>/', DisponibilidadeView.as_view(), name='disponibilidade'),
+    path('', include(router.urls)),  # gera /services/ e /bookings/
     path('professionals/', ProfessionalListAPIView.as_view(), name='professional-list'),
+    path('disponibilidade/<int:service_id>/', DisponibilidadeView.as_view(), name='disponibilidade'),
     path('criar_reserva/', criar_reserva, name='criar_reserva'),
     path('booking_detail/', booking_detail, name='booking_detail'),
     path('enviar_confirmacao/', enviar_confirmacao_email, name='enviar_confirmacao_email'),
