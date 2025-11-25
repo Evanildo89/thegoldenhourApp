571094e1985e44598347a4c3ce13245da49bdcfd
Evanildo89 <vany_casal@hotmail.com>
2025-11-25 17:20:56 +0000

    Atualiza settings.py com SendGrid

--- landing_app/backend/settings.py
+++ landing_app/backend/settings.py
@@ -100,14 +100,10 @@ else:
     }
 
 # Emails
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-ADMIN_EMAIL = config('ADMIN_EMAIL', default=EMAIL_HOST_USER)
+EMAIL_BACKEND = "sgbackend.SendgridBackend"
+SENDGRID_API_KEY = config("SENDGRID_API_KEY")
+DEFAULT_FROM_EMAIL = "evanildovrodrigues@gmail.com"
+ADMIN_EMAIL = "evanildovrodrigues@gmail.com"
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
